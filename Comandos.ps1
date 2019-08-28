ed6b4477fe45dfc939819295edb97614731887ca
Modolin <ssoliveira@gmail.com>
2019-08-28 11:37:31 -0300

    Atualização V4

--- Comandos.ps1
+++ Comandos.ps1
@@ -3,3 +3,29 @@
 ## Alterando a porta para executar comandos online
  git config --global http.proxy winp:8080 
  git config --global http.proxy http[s]://userName(encoded):password(encoded)@proxyaddress:port
+
+
+## Lista as Configuracoes
+ git config --list
+
+ git config --list --show-origin
+
+ git config user.name
+ John Doe
+
+#Checking the Status of Your Files
+ git status
+
+  #Short Status
+  git status -s
+
+# Files Actions
+  #Add Arquivos
+   git add Comandos.txt
+  #Rename Files
+  git mv Comandos.txt Comandos.ps1  
+  #Restore File to Unstaged
+  git restore --staged arquivoIgnore.js 
+
+  #Publica no repositorio online
+  git push
