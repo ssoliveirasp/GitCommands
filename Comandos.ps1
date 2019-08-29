7deef1382ae4c9340d25c48cf212940f5a5923f7
Modolin <ssoliveira@gmail.com>
2019-08-29 17:05:29 -0300

    Ajustado a estrutura de pastas.

--- Comandos.ps1
+++ Comandos.ps1
@@ -4,6 +4,9 @@
  git config --global http.proxy winp:8080 
  git config --global http.proxy http[s]://userName(encoded):password(encoded)@proxyaddress:port
 
+#Definir editor
+ #https://uw-madison-aci.github.io/git-novice/02-setup/
+ git config --global core.editor "code --wait" #vscode 
 
 ## Lista as Configuracoes
  git config --list
@@ -46,3 +49,35 @@
     git commit -m "Remove Diretorio"
     git rm --cached -r  logs #-r (recursively) é utilizado para indicar que é um diretorio
     git push
+
+#Rename File / Moving Files
+git mv s.md FileRename.md
+git commit -m "File Renamed s.mf to FileRename.md"
+
+#Git Log
+
+  #Log pretty-format
+  git log --pretty=format:"%h - %an, %ar : %s" 
+
+  # Option Description of Output
+  # %H Commit hash
+  # %h Abbreviated commit hash
+  # %T Tree hash
+  # %t Abbreviated tree hash
+  # %P Parent hashes
+  # %p Abbreviated parent hashes
+  # %an Author name
+  # %ae Author email
+  # %ad Author date (format respects the --date=option)
+  # %ar Author date, relative
+  # %cn Committer name
+  # %ce Committer email
+  # %cd Committer date
+  # %cr Committer date, relative
+  # %s Subject
+
+  #Log --since
+   git log --since=2.weeks --pretty=format:"%h - %an, %ar : %s" 
+
+  git log --pretty="%h - %s" --author='Junio C Hamano' 
+          --since="2008-10-01" \ --before="2008-11-01" --no-merges -- t/
