17b036dc0e99257bc88ae2898c424664aa9770c3
ssoliveirasp <ssoliveira@gmail.com>
2020-05-22 13:56:11 -0300

    Update Comandos.ps1

--- Comandos.ps1
+++ Comandos.ps1
@@ -1,66 +1,104 @@
-
-
-## Alterando a porta para executar comandos online
- git config --global http.proxy winp:8080 
- git config --global http.proxy http[s]://userName(encoded):password(encoded)@proxyaddress:port
-
-#Definir editor
- #https://uw-madison-aci.github.io/git-novice/02-setup/
- git config --global core.editor "code --wait" #vscode 
-
-## Lista as Configuracoes
- git config --list
-
- git config --list --show-origin
-
- git config user.name
- John Doe
-
+*****************************************************
+* Status de Arquivo
+*****************************************************
 #Checking the Status of Your Files
- git status
+git status
+git status -s //Short Status
 
-  #Short Status
-  git status -s
+*****************************************************
+* Files Actions
+*****************************************************
+//Add Arquivos
+git add Comandos.txt 
+git add DeleteFile.js
 
-# Files Actions
+//Rename Files
+git mv Comandos.txt Comandos.ps1  
 
-  #Add Arquivos
-   git add Comandos.txt
-  #Rename Files
-  git mv Comandos.txt Comandos.ps1  
-  #Restore File to Unstaged
-  git restore --staged arquivoIgnore.js 
+//Restore File to Unstaged
+git restore --staged arquivoIgnore.js 
 
-  #Publica no repositorio online
-  git push
-  
-  # atualizar seu repositório local com a mais nova versão
-  git pull
-  
-  #Add\Remove File
+//Remove File
+git rm  DeleteFile.js         #Apaga do Repositorio e Localmente
+git rm --cached DeleteFile.js #Apaga somente do Repositório
 
-    #Add/Remove File
-    git add DeleteFile.js
-    git rm  DeleteFile.js         #Apaga do Repositorio e Localmente
-    git rm --cached DeleteFile.js #Apaga somente do Repositório
-
-    #Add/Remove specific file in Directory
-    git add log\*.log
-    git rm --cached logs\*.log
-
-    #Add todos os arquivos de um diretorio
-    git add *
- 
-    #Add\Remove Directory
-    git add logs
-    git commit -m "Remove Diretorio"
-    git rm --cached -r  logs #-r (recursively) é utilizado para indicar que é um diretorio
-    git push
 
 #Rename File / Moving Files
 git mv s.md FileRename.md
 git commit -m "File Renamed s.mf to FileRename.md"
 
+//Add specific file in Directory
+git add log\*.log
+
+*****************************************************
+* Directory Actions
+*****************************************************
+//Remove specific file in Directory
+git rm --cached logs\*.log
+
+//Add todos os arquivos de um diretorio
+git add *
+
+//#Add\Remove Directory
+git add logs
+git commit -m "Remove Diretorio"
+git rm --cached -r  logs #-r (recursively) é utilizado para indicar que é um diretorio
+git push
+
+*****************************************************
+* Branch 
+*****************************************************
+
+//Cria uma nova Branch
+git branch newBranch
+
+//acesso novo branch
+git checkout newBranch    
+
+//remova o branch da seguinte forma
+git branch -d newBranch   
+
+/envia o branch para seu repositório remoto
+git push origin newBranch /
+
+
+*****************************************************
+* Merge 
+*******************************************
+#Merge
+
+//fazer o merge das alterações, você pode também pré-visualizá-as usando
+git diff <branch origem> <branch destino> 
+
+//merge de um outro branch ao seu branch ativo (ex. master), use
+git merge <branch>                       
+
+//Fix the last commit message
+git commit --amend or git commit --amend -m "Fixes bug #42"
+
+*****************************************************
+* Check-in and Check-out 
+*******************************************
+
+//Undo checkout
+git checkout -q --Arquivo.txt
+git reset --hard
+
+//Git Revert - If you have already committed files but realised you made a mistake then the command 
+git revert HEAD~1
+
+*****************************************************
+* Atualização e Publicação 
+*****************************************************
+//Publica no repositorio online
+git push
+  
+//Atualizar seu repositório local com a mais nova versão
+git pull
+
+*****************************************************
+* Git Log
+*****************************************************
 #Git Log
 
   #Log pretty-format
@@ -89,27 +127,23 @@ git commit -m "File Renamed s.mf to FileRename.md"
   git log --pretty="%h - %s" --author='Junio C Hamano' 
           --since="2008-10-01" \ --before="2008-11-01" --no-merges -- t/
 
-#Branch
-git branch newBranch
-git checkout newBranch    //acesso novo branch
-git branch -d newBranch   //remova o branch da seguinte forma
-git push origin newBranch //envia o branch para seu repositório remoto
-
-#Merge
-git diff <branch origem> <branch destino> //fazer o merge das alterações, você pode também pré-visualizá-as usando
-git merge <branch>                        //merge de um outro branch ao seu branch ativo (ex. master), use
-
-#Fix the last commit message
-git commit --amend or git commit --amend -m "Fixes bug #42"
-
-#Undo checkout
-git checkout -q --Arquivo.txt
-
-git reset --hard
-
-#Git Revert - If you have already committed files but realised you made a mistake then the command 
-git revert HEAD~1
-
-#quick overview of the commit history
+//quick overview of the commit history
 git log --oneline
+
+*****************************************************
+* Configurações
+*****************************************************
+
+## Alterando a porta para executar comandos online
+ git config --global http.proxy winp:8080 
+ git config --global http.proxy http[s]://userName(encoded):password(encoded)@proxyaddress:port
+
+#Definir editor
+ #https://uw-madison-aci.github.io/git-novice/02-setup/
+ git config --global core.editor "code --wait" #vscode 
+
+## Lista as Configuracoes
+ git config --list
+ git config --list --show-origin
+ git config user.name John Doe
 
